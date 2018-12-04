26aac1c4802c721296ffeb8bc2e9f0a17180ca92
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-12-03 23:20:16 -0800

    update 2.0

--- urbanDictionaryAPI.js
+++ urbanDictionaryAPI.js
@@ -1,63 +1,95 @@
 /*jshint esversion: 6 */
+
+/**
+ * Sets references to document objects onload
+ */
 const SubButton = document.getElementById("subButton");
 const PrintArea = document.getElementById("printHere");
+const UrbanWord = document.getElementById("InputUrbanWord");
 
-function getUrbanWord(urbanWord) {
-  const url = `https://api.urbandictionary.com/v0/define?term=${urbanWord}`;
-  var request = new XMLHttpRequest();
-  request.open("GET", url);
-  request.responseType = "json";
-  request.send();
-
-  request.onload = function(urbanResponse) {
-    var list = urbanResponse.target.response.list;
-    var printThis = "";
-
-    for (var i = 0; i < list.length; i++) {
-      
-  //     console.log(`
-  //        ${list[i].definition}
-  //        ${list[i].permalink}
-  //        ${list[i].thumbs_up}
-  //        ${list[i].author}
-  //        ${list[i].word}
-  //        ${list[i].defid}
-  //        ${list[i].current_vote}
-  //        ${list[i].written_on}
-  //        ${list[i].example}
-  //        ${list[i].thumbs_down}
-  //  `);
-
-      var card = `
+/**
+ * @param {*} json - json data
+ * This event renders the json data into a useable list.
+ */
+const renderJSON = (json) => {
+  if (json.list.length > 0) {
+    let rendered = json.list.map((x, index) => {
+      return (
+        `
+          <br>
+          <div class="card" key="${index}">
+          <div class="card-body">
+          <h5 class="card-title">${x.word}</h5>
+          <p class="card-text"> ${x.definition} </p>
+          <p class="card-text">Example: ${x.example}</p>
+          <a href="${x.permalink}" class="btn btn-primary">Source</a>
+          <br>
+          <small class="text-muted">${x.written_on} by ${x.author}</small>
+          </div>
+          </div>
+          `
+      );
+    });
+    return rendered;
+  }
+  else {
+    // this is the page that comes up with no results 
+    return (
+      `
       <br>
-   <div class="card">
-   <div class="card-body">
-
-   <h5 class="card-title">${list[i].word}</h5>
-   <p class="card-text"> ${list[i].definition} </p>
-   <p class="card-text">Example: ${list[i].example}</p>
-   <a href="${list[i].permalink}" class="btn btn-primary">Source</a>
-   <br>
-   <small class="text-muted">${list[i].written_on} by  ${
-        list[i].author
-      }  </small>
-
-   </div>
-   </div>
-   `;
-
-      printThis += card;
-    }
-
-    PrintArea.innerHTML = printThis;
-  };
+      <div class="card" key="${1}">
+      <div class="card-body">
+      <h5 class="card-title text-danger">Sorry that does not exist</h5>
+      <p class="card-text"> Sorry the word you are looking for does not exist try another word.</p>
+      <br>
+      </div>
+      </div>
+      `
+    )
+  }
 }
 
-// getUrbanWord('french');
+/**
+ * @param {*} word - word you want to search for 
+ * this function sends a get request to the Urban Dictionary API
+ */
+const getUrbWord = (word) => {
+  let url = `https://api.urbandictionary.com/v0/define?term=${word}`;
+  fetch(url)
+    .then(data => {
+      return data.json()
+    })
+    .then(json => {
+      console.log(json)
+      return renderJSON(json)
+    })
+    .then(rendered => {
+      PrintArea.innerHTML = rendered;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
 
+/**
+ * Triggered by pressing enter on input field
+ */
+UrbanWord.addEventListener("keyup", (event) => {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    SubButton.click()
+  }
+});
+
+/**
+ * Triggered on Button Click
+ */
 SubButton.onclick = () => {
-const UrbanWord = document.getElementById("InputUrbanWord");
-  var WordVal = UrbanWord.value;
-    getUrbanWord(`${WordVal}`);
- 
+  try {
+    let WordVal = UrbanWord.value;
+    getUrbWord(WordVal);
+  }
+  catch (err) {
+    console.log(err)
+  }
 };
